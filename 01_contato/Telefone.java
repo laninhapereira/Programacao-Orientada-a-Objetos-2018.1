61dc00674e54a56bb3b47a404fadbe7aff1f7935
Aurislania Pereira Batista <37314141+laninha73pereira@users.noreply.github.com>
2018-03-24 18:19:48 -0300

    Add files via upload

---
+++ 01_contato/Telefone.java
@@ -0,0 +1,18 @@
+package projectagenda;
+
+public class Telefone {
+
+	public String nome;
+	 public int tel;
+	 public int foneid;
+	 
+	 public Telefone(){
+	}
+	 
+	 public Telefone(String nome, int telefone, int foneid){
+	 this.nome = nome;
+	 this.tel = telefone;
+	 this.foneid = foneid;
+	}
+	 
+	}
