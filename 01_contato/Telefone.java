94ebcc74a800ed17f96241cd6dc10883854b8d76
Aurislania Pereira Batista <37314141+laninha73pereira@users.noreply.github.com>
2018-04-06 16:49:52 -0300

    Add files via upload

---
+++ 01_contato/Telefone.java
@@ -0,0 +1,14 @@
+package teste;
+
+public class Telefone {
+	
+	public String nome;
+	public String id;
+	public int telefone;
+	
+	public Telefone(String nome, int telefone, String id) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.id = id;
+	}
+}
