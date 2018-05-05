54272faeff9ba7b5059d4ca3b1e4de177cfa027e
Aurislania Pereira Batista <37314141+laninha73pereira@users.noreply.github.com>
2018-05-05 18:38:44 -0300

    Update Controller.java

--- 03_Agiota/src/Agiota/Controller.java
+++ 03_Agiota/src/Agiota/Controller.java
@@ -1,115 +1,117 @@
 package Agiota;
 
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 class Cliente {
-
-	public String clienteid;
+	
+	public String apelido;
 	public String nome;
+	public String esta = "vivo";
 	public float saldo = 0;
-	public String estado = "vivo";
 	
-	public Cliente(String clienteid, String nome) {
-		this.clienteid = clienteid;
-		this.nome =  nome;
+	public Cliente (String apelido, String nome) {
+		this.apelido = apelido;
+		this.nome = nome;
 	}
 	
-	public String toString() {
-		return " "+clienteid +":"+ nome+ ":" +estado + "\n";
+	public String toString(){
+		return " "+nome+" "+apelido+" "+esta;
 	}
 	
-	public String mostrarSaldoClientes() {
-		return " Nome:" + nome +" Saldo : " + saldo + "\n";
+	public String Saldo() {
+		return "nome: "+nome+"saldo: "+saldo;
 	}
-
 	
 }
 
 class Sistema{
-	
 	public float saldo = 0;
-	public float dinheiro;
-	public int id_transacao = 0;
+	public float dinheiro = 0;
+	public int id_transacao;
 	public int id = 0;
 	
 	ArrayList<Cliente> clientes;
 	ArrayList<Transacao> transacoes;
 	
-	public Sistema(float dinheiro) {
+	public Sistema (float dinheiro) {
 		this.dinheiro = dinheiro;
 		clientes = new ArrayList<Cliente>();
 		transacoes =  new ArrayList<Transacao>();
-		saldo = saldo + dinheiro;
-	}
-
-	public String toString() {
-		return "Sistema iniciado com: " + saldo + " " + clientes.toString() + "\n";
-	}
-
-	public void cadastrarCliente(String nome, String clienteid) {
-		for (Cliente c1 : clientes)
-			if (c1.nome.equals(nome))
-				throw new RuntimeException("Pessoa já cadastrada!");
-
-		clientes.add(new Cliente(clienteid, nome));
-	}
-
-
-	public void cadastrarDivida(String nome, float valor) {
-	   if(valor <= saldo) {
-		  saldo = saldo + valor;
-	      this.transacoes.add(new Transacao(nome,valor, id));
-	      id++;
-	      return;
-	   }
-	   else
-		   throw new RuntimeException ("Saldo inferior ao dinheiro requerido!");
-	   
+		saldo = saldo+dinheiro;
 	}
 	
-	public void emprestimo(String nome, float valor) {
-		for (Cliente c1 : clientes)
-			if (c1.nome.equals(nome) ) { 
-				c1.saldo = valor + c1.saldo;
-				this.cadastrarDivida(nome, valor);
+	public String toString () {
+		return "Sistema iniciado com: "+saldo+" "+clientes.toString();
+	}
+	
+	public void CadastrarCliente(String nome, String apelido) {
+		for (Cliente c : clientes) {
+			if (c.nome.equals(nome))
+				throw new RuntimeException("Pessoa jÃ¡ cadastrada!");
+			
+		clientes.add(new Cliente(apelido, nome));	
+		}
+	}
+	
+	public void CadastrarDivida(String nome, float valor) {
+		if (valor <= saldo) {
+			saldo = saldo + valor;
+			
+			this.transacoes.add(new Transacao(nome, valor, id));
+			id ++;
+			return;
+		} 
+		else {
+			throw new RuntimeException ("Saldo inferior ao dinheiro requerido!");
+		}
+			
+	}
+	
+	public void Emprestimo (String nome, float valor) {
+		for (Cliente c : clientes) {
+			if (c.nome.equals(nome) ) { 
+				c.saldo = valor + c.saldo;
+				this.CadastrarDivida(nome, valor);
 		        return;
-	        }
+			}
+		}
 		
 		throw new RuntimeException("Pessoa ou saldo invalido!");
 	}
 	
-	 public String mostrarClientes() {
-	    	String saida =  "";
-	    	for(int i = 0; i < clientes.size(); i++)
-	    	   saida += ""+ this.clientes.get(i).mostrarSaldoClientes();
-	    	return saida;
-	    }
-	 
-	 public void mostrarDeterminadoCliente(String nome) {
-			int i = 0;
-			for (Cliente c : clientes) {
-				if (c.nome.equals(nome)) {
-					System.out.println(c.mostrarSaldoClientes());
-					while(transacoes.get(i).nome.equals(nome)) {
-						       System.out.println(transacoes.get(i).toString());
-			                   i++;       
-					}
-				}
-			return;
-			}
-		    throw new RuntimeException("Cliente não encontrado!");
-		}
-
-	
-	public String mostrarTransacoes() {
-			 return "" + transacoes.toString();
+	public String MostrarClientes () {
+		String s = " ";
+		for(int i = 0; i < clientes.size(); i++) 
+	    	   s += ""+ this.clientes.get(i).Saldo();
+	    	return s;
 	}
-
- 
-	public void pagarDebito(String nome, float valor) {
-		for (Cliente c : clientes)
+	
+	public void MostrarDeterminadoCliente (String nome) {
+		int i = 0;
+		for (Cliente c : clientes) {
+			if (c.nome.equals(nome)) {
+				System.out.println(c.Saldo());
+				while(transacoes.get(i).nome.equals(nome)) {
+					 System.out.println(transacoes.get(i).toString());
+	                   i++; 
+				}
+			}
+			
+			return;
+			
+		}
+		
+		throw new RuntimeException("Cliente nÃ£o encontrado!");
+		
+	}
+	
+	public String MostrarTransacoes () {
+		return " " + transacoes.toString();
+	}
+	
+	public void PagarDebito (String nome, float valor) {
+		for (Cliente c : clientes) {
 			if (c.nome.equals(nome)) {
 				if (c.saldo < 0) {
 					c.saldo = saldo + valor;
@@ -119,56 +121,56 @@ class Sistema{
 					return;
 				}
 			}
-
-		throw new RuntimeException("Cliente não encontrado!");
-
+		}
+		
+		throw new RuntimeException("Cliente nÃ£o encontrado!");
+		
 	}
-
-	public void matarCliente(String nome) {
+	
+	public void MatarCliente(String nome) {
 		for( int i = 0; i < clientes.size(); i++) {
 			if(clientes.get(i).nome.equals(nome)) {
 				this.clientes.remove(clientes.get(i));
-				apagarDividas(nome);
+				ApagarDividas(nome);
 		        return;
 			}
 		}
 		
-		throw new RuntimeException("Cliente não encontrado!");
+		throw new RuntimeException("Cliente nÃ£o encontrado!");
+		
 	}
- 
-	public void apagarDividas( String nome) {
-		for(int i = 0 ; i<transacoes.size(); i++) {
-			if(transacoes.get(i).nome.equals(nome))
+	
+	public void ApagarDividas (String nome) {
+		for (int i = 0; i < transacoes.size(); i++) {
+			if(transacoes.get(i).nome.equals(nome)) {
 				this.transacoes.remove(transacoes.get(i));
 			    i--;
+			}
 		}
 	}
 	
 }
 
 
-class Transacao{
-
+class Transacao {
 	public String nome;
-	public float valordebito;
+	public float valorDebito;
 	public int id = 0;
-	public float valortotal = 0;
+	public float total = 0;
 	
 	public Transacao(String nome, float valor, int id) {
 		this.nome = nome;
-		this.valordebito = valor;
+		this.valorDebito = valor;
 		this.id = id;
-		this.valortotal = valordebito + valortotal;
+		this.total = valorDebito + total;
 	}
 	
 	public String toString() {
-		return  " id:" + id + " nome:" + nome + " valor:" + valordebito +"\n";
+		return "id:"+id+" nome:"+nome+" valor:"+valorDebito;
 	}
-
+	
 }
 
-
-	
 public class Controller {
 	Sistema sis;
 	Scanner sca;
@@ -182,31 +184,28 @@ public class Controller {
 	public String query(String line) {
 		String[] ui = line.split(" ");
 		
-	    if (ui[0].equals("iniciar"))
+	    if (ui[0].equals("Iniciar"))
 			sis = new Sistema(Float.parseFloat(ui[1]));
-	    else if (ui[0].equals("mostrar"))
+	    else if (ui[0].equals("Mostrar"))
 			return " " + sis;
-	    else if(ui[0].equals("cadastrarCliente"))
-	    	sis.cadastrarCliente(ui[1],ui[2]);
-	    else if(ui[0].equals("mostrarTrasacoes"))
-	    	return "" + sis.mostrarTransacoes() ;
-	    else if(ui[0].equals("emprestimo"))
-	    	sis.emprestimo(ui[1],Float.parseFloat(ui[2]));
-	    else if(ui[0].equals("mostrarClientes"))
-	    	return ""+ sis.mostrarClientes();
-	    else if(ui[0].equals("mostrarDeterminadoCliente"))
-	        sis.mostrarDeterminadoCliente(ui[1]);
-	    else if(ui[0].equals("receberDebito"))
-	    	sis.pagarDebito(ui[1],Float.parseFloat(ui[2]));
-	    else if(ui[0].equals("matarCliente"))
-	    	sis.matarCliente(ui[1]);
+	    else if(ui[0].equals("CadastrarCliente"))
+	    	sis.CadastrarCliente(ui[1],ui[2]);
+	    else if(ui[0].equals("MostrarTrasacoes"))
+	    	return "" + sis.MostrarTransacoes() ;
+	    else if(ui[0].equals("Emprestimo"))
+	    	sis.Emprestimo(ui[1],Float.parseFloat(ui[2]));
+	    else if(ui[0].equals("MostrarClientes"))
+	    	return ""+ sis.MostrarClientes();
+	    else if(ui[0].equals("MostrarDeterminadoCliente"))
+	        sis.MostrarDeterminadoCliente(ui[1]);
+	    else if(ui[0].equals("ReceberDebito"))
+	    	sis.PagarDebito(ui[1],Float.parseFloat(ui[2]));
+	    else if(ui[0].equals("MatarCliente"))
+	    	sis.MatarCliente(ui[1]);
 	    else
 	    	return " Comando invalido";
 		return "done";
 	}
-
-
-
 
 	public void shell() {
 		while (true) {
